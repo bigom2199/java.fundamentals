bd4ca4b2a4a848624fbeda1f41f4874d5f967937
bigom2199 <138991838+bigom2199@users.noreply.github.com>
2023-07-13 08:18:19 +0200

    scopesExamples

--- src/main/java/lexicon/se/ScopesExamples.java
+++ src/main/java/lexicon/se/ScopesExamples.java
@@ -5,21 +5,23 @@ package lexicon.se;
 
 public class ScopesExamples {// start of class scope
 
+
+    static int centyry=100; // Field variable (variable (variable in class Scope)
+
     public static void main(String[] args) {
 
-    }
-    static int century=100;
+    //    printMenu();
+      //      miniLoop();
+        System.out.printLn("century = " + century);
 
-    public static int getCentury() {
-        return century;
-    }
-    //   PRINTMenu();
+        public static void miniLOOp(){
+            // variable accessible in `miniLoop()` nowhere else.-Local Scope
+        int result =0;
+        //
+        for (int i=0,i=2;i<5;i++,i++=){//Block scope
 
-    //    miniloop();
-
-
-
-
+            result =result +i;
+        }
 
 } // End of class Scope
 
